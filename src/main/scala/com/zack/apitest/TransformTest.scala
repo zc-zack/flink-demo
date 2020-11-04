d4ea0bb89e946990def353819316ee7b50d2e362
张成 <zc@yunshan.cn>
2020-11-04 16:05:19 +0800

    算子运算

--- src/main/scala/com/zack/apitest/TransformTest.scala
+++ src/main/scala/com/zack/apitest/TransformTest.scala
@@ -1,7 +1,7 @@
 package com.zack.apitest
 
-import org.apache.flink.api.scala._
-import org.apache.flink.streaming.api.scala.DataStream
+import org.apache.flink.api.common.functions.ReduceFunction
+import org.apache.flink.streaming.api.scala._
 
 /**
  * com.zack.apitest
@@ -10,14 +10,36 @@ import org.apache.flink.streaming.api.scala.DataStream
  * @version 1.0
  * @create 2020/11/3 14:33 
  */
-class TransformTest {
+object TransformTest {
   def main(args: Array[String]): Unit = {
-    val env = ExecutionEnvironment.getExecutionEnvironment
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    val streamFromFile = env.readTextFile("D:\\IdeaProjects\\flink-demo\\src\\main\\resources\\sensor.txt");
+    val inputSteam = env.readTextFile("D:\\IdeaProjects\\flink-demo\\src\\main\\resources\\sensor.txt");
+    env.setParallelism(1)
+    //样例转换
+    val dataStream = inputSteam.map(data => {
+      val arr = data.split(",")
+      SensorReading(arr(0).trim, arr(1).trim.toLong, arr(2).trim.toDouble)
+    })
 
+    // 分组聚合，输出每个传感器最小值
+    val aggStream = dataStream
+      .keyBy("id")//根据id进行分组
+      .minBy("temperature")
 
+    //需要输出当前最小的温度值以及最近的时间戳
+    val resultStream = dataStream
+        .keyBy("id")
+        .reduce((currentState, newData) =>
+          SensorReading(currentState.id, newData.timestamp, currentState.temperature.min(newData.temperature))
+        )
 
+    aggStream.print()
     env.execute("Transform test")
   }
+}
+
+class MyReduceFunction extends ReduceFunction[SensorReading] {
+  override def reduce(t: SensorReading, t1: SensorReading): SensorReading =
+    SensorReading(t.id, t1.timestamp, t.temperature.min(t1.temperature))
 }
