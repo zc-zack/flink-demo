67b7bb67089d7ae3a001c058d466c68edcf9d542
张成 <zc@yunshan.cn>
2020-11-05 14:23:14 +0800

    函数操作

--- src/main/scala/com/zack/apitest/TransformTest.scala
+++ src/main/scala/com/zack/apitest/TransformTest.scala
@@ -1,7 +1,9 @@
 package com.zack.apitest
 
-import org.apache.flink.api.common.functions.ReduceFunction
+import org.apache.flink.api.common.functions.{FilterFunction, ReduceFunction, RichMapFunction}
 import org.apache.flink.streaming.api.scala._
+
+import Array._
 
 /**
  * com.zack.apitest
@@ -21,6 +23,7 @@ object TransformTest {
       val arr = data.split(",")
       SensorReading(arr(0).trim, arr(1).trim.toLong, arr(2).trim.toDouble)
     })
+      //.filter(new MyFilter)
 
     // 分组聚合，输出每个传感器最小值
     val aggStream = dataStream
@@ -33,8 +36,38 @@ object TransformTest {
         .reduce((currentState, newData) =>
           SensorReading(currentState.id, newData.timestamp, currentState.temperature.min(newData.temperature))
         )
+    dataStream.keyBy("id").reduce(new MyReduceFunction)
 
-    aggStream.print()
+
+
+    //多流转换操作
+    //将传感亲分为低温和高温
+    val splitStream = dataStream
+      .split(data => {
+        if(data.temperature > 30.0) Seq("high") else Seq("low")
+      })
+    val highTempStream = splitStream.select("high");
+    val lowTempStream = splitStream.select("low")
+    val allTempStream = splitStream.select("high", "low")
+
+//    highTempStream.print("high")
+//    lowTempStream.print("low")
+//    allTempStream.print("all")
+    val warningStream = highTempStream.map(data => (data.id, data.temperature))
+    val connectedStreams = warningStream.connect(lowTempStream)
+
+    //用coMap对数据进行分别处理
+    val coMapResultStream = connectedStreams
+        .map(
+          warningData => (warningData._1, warningData._2, "waring"),
+          lowTempData => (lowTempData.id, "healthy")
+        )
+
+    //coMapResultStream.print()
+
+    val unionStream = highTempStream.union(lowTempStream, allTempStream)
+
+
     env.execute("Transform test")
   }
 }
@@ -42,4 +75,16 @@ object TransformTest {
 class MyReduceFunction extends ReduceFunction[SensorReading] {
   override def reduce(t: SensorReading, t1: SensorReading): SensorReading =
     SensorReading(t.id, t1.timestamp, t.temperature.min(t1.temperature))
+}
+
+// 自定义函数类
+class MyFilter extends FilterFunction[SensorReading] {
+  override def filter(t: SensorReading): Boolean =
+    t.id.startsWith("sensor_1")
+}
+
+//富函数，可以获得运行时上下文，还有一些生命周期
+class MyRichMapper extends RichMapFunction[SensorReading, String] {
+  override def map(in: SensorReading): String =
+    in.id + "temperature"
 }
