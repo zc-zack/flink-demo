a7021322460f092cebee60c2bdf2a3d4cc8cf755
张成 <zc@yunshan.cn>
2020-11-06 15:41:54 +0800

    窗口操作

--- src/main/scala/com/zack/apitest/WindowTest.scala
+++ src/main/scala/com/zack/apitest/WindowTest.scala
@@ -1,5 +1,6 @@
 package com.zack.apitest
 
+import org.apache.flink.api.common.functions.ReduceFunction
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.assigners.{EventTimeSessionWindows, SlidingEventTimeWindows, TumblingEventTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
@@ -16,8 +17,10 @@ object WindowTest {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
 
-    val inputPath = "D:\\IdeaProjects\\flink-demo\\src\\main\\resources\\sensor.txt"
-    val inputStream = env.readTextFile(inputPath)
+//    val inputPath = "D:\\IdeaProjects\\flink-demo\\src\\main\\resources\\sensor.txt"
+//    val inputStream = env.readTextFile(inputPath)
+
+    val inputStream = env.socketTextStream("192.168.1.52", 7777);
 
     val dataStream = inputStream
       .map( data => {
@@ -25,14 +28,24 @@ object WindowTest {
         SensorReading(arr(0).trim, arr(1).trim.toLong, arr(2).trim.toDouble)
       } )
 
+    // 每15秒统计一次，窗口内各传感器的温度值的最小值
     val resultStream = dataStream
-        .map( data => (data.id, data.temperature) )
+        .map( data => (data.id, data.temperature, data.timestamp) )
         .keyBy(_._1) //按照二元组的第一个元素(id)进行分组
        // .window(TumblingEventTimeWindows.of(Time.seconds(15))) //滚动时间窗口
         //.window(SlidingEventTimeWindows.of(Time.seconds(15), Time.seconds(3)))  //滑动时间窗口
 //        .window(EventTimeSessionWindows.withGap(Time.seconds(15)))  //会话窗口
 //        .timeWindow(Time.seconds(15))
-        .countWindow(10)
+       // .countWindow(10)  //滚动计数窗口
+        .timeWindow(Time.seconds(15))
+        .reduce( (currentResult, newData) => (currentResult._1, currentResult._2.min(newData._2), newData._3) )
+    resultStream.print()
     env.execute("window test")
+  }
+}
+
+class MyReducer extends ReduceFunction[SensorReading] {
+  override def reduce(t: SensorReading, t1: SensorReading): SensorReading = {
+    SensorReading(t.id, t1.timestamp, t.temperature.min(t1.temperature))
   }
 }
