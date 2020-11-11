b86262eeea37ee253e95d5629c907f1d30a80811
张成 <zc@yunshan.cn>
2020-11-11 15:37:26 +0800

    从文件读取数据

---
+++ src/main/scala/com/zack/apitest/tableapi/TableApiTest.scala
@@ -0,0 +1,66 @@
+package com.zack.apitest.tableapi
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.{DataTypes, EnvironmentSettings, Table, TableEnvironment}
+import org.apache.flink.table.api.bridge.scala._
+import org.apache.flink.table.descriptors.{FileSystem, OldCsv, Schema}
+
+/**
+ * com.zack.apitest.tableapi
+ *
+ * @author zhangc
+ * @version 1.0
+ * @create 2020/11/11 14:58 
+ */
+object TableApiTest {
+  def main(args: Array[String]): Unit = {
+    //1.创建环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    val tableEnv = StreamTableEnvironment.create(env)
+
+    /*//1.1 基于老版本planner的流处理
+    val settings = EnvironmentSettings.newInstance()
+      .useOldPlanner()
+      .inStreamingMode()
+      .build()
+    val oldStreamTableEnv = StreamTableEnvironment.create(env, settings)
+
+    //1.2基于老版本的批处理
+    val batchEnv = ExecutionEnvironment.getExecutionEnvironment
+    val oldBatchTableEnv = BatchTableEnvironment.create(batchEnv)
+
+    // 1.3 基于blink planner流处理
+    val blinkStreamSettings = EnvironmentSettings.newInstance()
+      .useBlinkPlanner()
+      .inStreamingMode()
+      .build()
+    val blinkStreamTableEnv = StreamTableEnvironment.create(env, blinkStreamSettings)
+
+    // 1.4 基于blink planner批处理
+    val blinkBatchSettings = EnvironmentSettings.newInstance()
+      .useBlinkPlanner()
+      .inBatchMode()
+      .build()
+    val blinkBatchTableEnv = TableEnvironment.create(blinkBatchSettings)*/
+
+    //2.连接外部系统，读取数据，注册表
+    //2.1读取文件
+    val filePath = "D:\\IdeaProjects\\flink-demo\\src\\main\\resources\\sensor.txt"
+    tableEnv.connect(new FileSystem().path(filePath))
+      .withFormat(new OldCsv())
+      .withSchema(new Schema()
+        .field("id", DataTypes.STRING())
+        .field("timestamp", DataTypes.BIGINT())
+        .field("temperature", DataTypes.DOUBLE()))
+      .createTemporaryTable("inputTable")
+
+    val inputTable: Table = tableEnv.from("inputTable")
+    inputTable.toAppendStream[(String, Long, Double)].print()
+
+    env.execute("table api test")
+
+  }
+}
